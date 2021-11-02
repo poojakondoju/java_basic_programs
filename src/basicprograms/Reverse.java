ae99647f85e889ab9b9a4b91fbbd5ddff38c1a28
poojakondoju <kondojupoojitha612@gmail.com>
2021-11-02 14:32:39 +0530

    basic programs

---
+++ src/basicprograms/Reverse.java
@@ -0,0 +1,27 @@
+package basicprograms;
+
+import java.util.Scanner;
+
+public class Reverse {
+	// Basic Java program that reverses an array
+
+	public static void reverseNumber(int number) {
+		if (number < 10) {
+			// prints the same number if the number is less than 10
+			System.out.println(number);
+			return;
+		} else {
+			System.out.print(number % 10);
+			reverseNumber(number / 10);
+		}
+	}
+
+	public static void main(String args[]) {
+		System.out.print("Enter the number that you want to reverse: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.print("The reverse of the given number is: ");
+		// method calling
+		reverseNumber(num);
+	}
+}
